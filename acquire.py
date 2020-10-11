5f6ab4fd0e34fd02ba040e6bba6b8e952e61ae3a
datastraine <anthony.straine@gmail.com>
2020-10-11 13:16:45 -0500

    Updates acquire files with proper sql

--- acquire.py
+++ acquire.py
@@ -18,71 +18,13 @@ def load_zillow_data():
     '''
     db = 'zillow'
     sql_query = '''
-    select 
-		zillow.properties_2017.parcelid,
-		basementsqft,
-		bathroomcnt,
-		bedroomcnt,
-		buildingqualitytypeid,
-		calculatedbathnbr,
-		decktypeid,
-		finishedfloor1squarefeet,
-		calculatedfinishedsquarefeet,
-		finishedsquarefeet12,
-		finishedsquarefeet13,
-		finishedsquarefeet15,
-		finishedsquarefeet50,
-		finishedsquarefeet6,
-		fips,
-		fireplacecnt,
-		fullbathcnt,
-		garagecarcnt,
-		garagetotalsqft,
-		hashottuborspa,
-		latitude,
-		longitude,
-		lotsizesquarefeet,
-		poolcnt,
-		poolsizesum,
-		pooltypeid10,
-		pooltypeid2,
-		pooltypeid7,
-		propertycountylandusecode,
-		propertyzoningdesc,
-		rawcensustractandblock,
-		regionidcity,
-		regionidcounty,
-		regionidneighborhood,
-		regionidzip,
-		roomcnt,
-		threequarterbathnbr,
-		unitcnt,
-		yardbuildingsqft17,
-		yardbuildingsqft26,
-		yearbuilt,
-		numberofstories,
-		fireplaceflag,
-		structuretaxvaluedollarcnt,
-		taxvaluedollarcnt,
-		assessmentyear,
-		landtaxvaluedollarcnt,
-		taxamount,
-		taxdelinquencyflag,
-		taxdelinquencyyear,
-		censustractandblock,
-		airconditioningdesc,
-		architecturalstyledesc,
-		buildingclassdesc,
-		heatingorsystemdesc,
-		propertylandusedesc,
-		storydesc,
-		typeconstructiondesc
-		logerror,
-		recent.transactiondate
-	from properties_2017
+select * from properties_2017
 	join (select parcelid, max(transactiondate) as transactiondate from predictions_2017
 	group by parcelid) recent
 	using(parcelid)
+	join (select parcelid, logerror, transactiondate from predictions_2017) est
+	on est.parcelid = recent.parcelid
+	and est.transactiondate = recent.transactiondate
 	left outer join zillow.airconditioningtype
 	using(airconditioningtypeid)
 	left outer join zillow.architecturalstyletype
@@ -98,13 +40,13 @@ def load_zillow_data():
 	left outer join zillow.buildingclasstype
 	using(buildingclasstypeid)
 	where latitude is not null 
-	and longitude is not null
+	and longitude is not null;
 	'''
-    file = 'zillow.csv'
+    file = 'zillow_full.csv'
     
     if os.path.isfile(file):
-        return pd.read_csv('zillow.csv')
+        return pd.read_csv('zillow_full.csv')
     else:
         df = pd.read_sql(sql_query, get_connection(db))
-        df.to_csv('zillow.csv', index=False)
+        df.to_csv('zillow_full.csv', index=False)
         return df
